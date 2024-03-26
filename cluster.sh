f960a236b4efc1883c69c054c6321df3697adbc9
zhanglikun <x@zahui.fan>
2024-03-27 00:32:20 +0800

    去掉git依赖

--- cluster.sh
+++ cluster.sh
@@ -11,13 +11,14 @@ ssh root@${MASTER2} "cd /tmp/init/ && bash ./init.sh"
 ssh root@${MASTER3} "cd /tmp/init/ && bash ./init.sh"
 
 # 生成keepalived配置文件
-git checkout keepalived/*.conf
-sed -e "s/__MASTER1__/${MASTER1}/g" \
-    -e "s/__MASTER2__/${MASTER2}/g" \
-    -e "s/__MASTER3__/${MASTER3}/g" \
-    -e "s/__VIP__/${API_SERVER}/g" \
-    -e "s/__NETWORK_NIC__/${NETWORK_NIC}/g" \
-    -i keepalived/*.conf
+for i in $(ls keepalived/keepalived*.conf); do
+    cat ${i} | sed -e "s/__MASTER1__/${MASTER1}/g" \
+        -e "s/__MASTER2__/${MASTER2}/g" \
+        -e "s/__MASTER3__/${MASTER3}/g" \
+        -e "s/__VIP__/${API_SERVER}/g" \
+        -e "s/__NETWORK_NIC__/${NETWORK_NIC}/g" \
+        > ${i}.tmp
+done
 
 # 分发仓库文件
 rsync -avz --exclude=temp * root@${MASTER1}:/tmp/
@@ -25,9 +26,9 @@ rsync -avz --exclude=temp * root@${MASTER2}:/tmp/
 rsync -avz --exclude=temp * root@${MASTER3}:/tmp/
 
 # keepalived 配置
-ssh root@${MASTER1} "cd /tmp/keepalived/ && bash install.sh && cp -r keepalived1.conf /etc/keepalived/keepalived.conf && systemctl restart keepalived"
-ssh root@${MASTER2} "cd /tmp/keepalived/ && bash install.sh && cp -r keepalived2.conf /etc/keepalived/keepalived.conf && systemctl restart keepalived"
-ssh root@${MASTER3} "cd /tmp/keepalived/ && bash install.sh && cp -r keepalived3.conf /etc/keepalived/keepalived.conf && systemctl restart keepalived"
+ssh root@${MASTER1} "cd /tmp/keepalived/ && bash install.sh && cp -r keepalived1.conf.tmp /etc/keepalived/keepalived.conf && systemctl restart keepalived"
+ssh root@${MASTER2} "cd /tmp/keepalived/ && bash install.sh && cp -r keepalived2.conf.tmp /etc/keepalived/keepalived.conf && systemctl restart keepalived"
+ssh root@${MASTER3} "cd /tmp/keepalived/ && bash install.sh && cp -r keepalived3.conf.tmp /etc/keepalived/keepalived.conf && systemctl restart keepalived"
 
 # 安装Docker
 ssh root@${MASTER1} "cd /tmp/docker/ && bash ./install.sh"
@@ -45,7 +46,7 @@ ssh root@${MASTER2} "cd /tmp/flannel/ && bash install.sh"
 ssh root@${MASTER3} "cd /tmp/flannel/ && bash install.sh"
 
 # 创建集群
-ssh root@${MASTER1} "cd /tmp/kubeadm && ./create_cluster.sh ${API_SERVER} | tee /tmp/install.log"
+ssh root@${MASTER1} "cd /tmp/kubeadm && bash ./create_cluster.sh ${API_SERVER} | tee /tmp/install.log"
 
 # 获取安装信息
 MASTER_JOIN_COMMAND=$(ssh root@${MASTER1} 'cat /tmp/install.log | grep -A 5 "You can now join any number of the control-plane" | grep -vE "You can now join any number of the control-plane|^$."')
